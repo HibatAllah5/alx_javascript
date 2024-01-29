8da2bb8fe1db9a4e8c83d1838972e184f5c801eb
HibatAllah5 <hibakhaldoune5@gmail.com>
2024-01-29 02:11:27 +0100

    new

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,10 @@
-//script that writes 3 lines
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+//script that writes 3 lines.
+
+const firstline = 'C is fun';
+const secondline = 'Python is cool';
+const thirdline = 'Javascript is amazing';
+
+console.log(firstline);
+console.log(secondline);
+console.log(thirdline);
+
