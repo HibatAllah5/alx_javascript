6abd6acc8a3e019abf12b997aab2955d779e374b
HibatAllah5 <hibakhaldoune5@gmail.com>
2024-02-07 15:35:24 +0100

    new

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+function welcome(fullName) {
+    let fullName = ('firstName' ,'lastName');
+    function displayFullName() {
+        console.log('Welcome' + ''+ displayFullName +!)
+    }
+}
