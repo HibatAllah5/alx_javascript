d560c1b550e211b6c2df698c6c7bc0589d77e5a8
HibatAllah5 <hibakhaldoune5@gmail.com>
2024-02-21 00:53:19 +0100

    ^^

---
+++ html_js_validation/1-email-validation.js
@@ -0,0 +1,16 @@
+function ValidateEmail(inputText) 
+{
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(inputText.value.match(mailformat))
+    {
+        alert("Valid email address!");
+        document.form1.text1.focus();
+        return true;
+    }
+    else
+    {
+        alert("Please enter a valid email address.");
+        document.form1.text1.focus();
+        return false;
+    }
+}
